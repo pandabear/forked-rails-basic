aac07c86190b43bce2fc0a6fdc271a5328cd6851
Veli-Matti Luoto <veli-matti.luoto@eficode.fi>
2011-12-14 14:36:14 +0200

    AJAXnized the commenting thingy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def new
-    @book = Book.find(params[:book_id])
+    @book    = Book.find(params[:book_id])
     @comment = @book.comments.new
 
     respond_to do |format|
@@ -10,16 +10,17 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @book = Book.find(params[:book_id])
+    @book    = Book.find(params[:book_id])
     @comment = @book.comments.build(params[:comment])
 
+    if @comment.save
+      flash[:notice] = "Comment saved"
+    else
+      flash[:error]  = "Comment couldn't be saved"
+    end
+
     respond_to do |format|
-      if @comment.save
-        format.html { redirect_to book_path(@book), notice: 'Comment was successfully created.' }
-      else
-        format.html { render action: "new" }
-      end
+      format.js
     end
   end
-
 end
