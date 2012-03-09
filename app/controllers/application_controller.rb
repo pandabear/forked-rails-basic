c670ad4d2b864df830221007e0ef8dc68dbb89f6
Omar Bakr <bakr.omar@gmail.com>
2012-03-09 16:43:43 +0200

    initial facebook implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # protect_from_forgery
+ 
+  before_filter :parse_facebook_cookies
+ def parse_facebook_cookies
+  @facebook_cookies = Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+ end
+
+  def index
+    graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
+    @email = graph.get_connections("me", "email")
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
+# Initialize the rails application 	
 Library::Application.initialize!
+
+# config/environment.rb
+# in Rails::Initializer.run do |config|
+#config.action_controller.allow_forgery_protection = false
+#config.gem "koala"
