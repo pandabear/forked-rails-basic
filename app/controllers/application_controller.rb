26956ed1d6ae6fb02fe03acd12fc934d93fba98f
Panda Bear <your@email.here>
2012-03-11 19:54:29 +0200

    Revert "initial facebook implementation"
    This reverts commit c670ad4d2b864df830221007e0ef8dc68dbb89f6.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery
- 
-  before_filter :parse_facebook_cookies
- def parse_facebook_cookies
-  @facebook_cookies = Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
- end
-
-  def index
-    graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
-    @email = graph.get_connections("me", "email")
-  end
-
+  protect_from_forgery
 end
